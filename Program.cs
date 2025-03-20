e21687c8c2803672db6fb7e82bbf7c06bab52003
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-20 10:05:55 +0700

    implementasi-generic-method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+class pemprosesanData
+{
+    public void DapatkanNilaiTerbesar <T>(T nilai1, T nilai2, T nilai3)
+    {
+        dynamic value_nilai1 = nilai1;
+        dynamic value_nilai2 = nilai2;
+        dynamic value_nilai3 = nilai3;
+        T temp;
+
+        if (value_nilai1 > value_nilai2 && value_nilai1 > value_nilai3)
+        {
+            temp = value_nilai1;
+        }
+        else if (value_nilai2 > nilai1 && value_nilai2 > value_nilai3)
+        {
+            temp = value_nilai2;
+        }
+        else
+        {
+            temp = value_nilai3;
+        }
+
+        System.Console.WriteLine(temp);
+    }
+}
+
+class program
+{
+    static void Main()
+    {
+        pemprosesanData nilaiTerbesar = new pemprosesanData();
+
+        nilaiTerbesar.DapatkanNilaiTerbesar<int>(12,34,56);
+    }
+}
